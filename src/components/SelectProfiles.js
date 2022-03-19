358a45767ed18ce13d765b64f972f0bab176bb06
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-03-19 00:55:09 -0600

    navbar

---
+++ src/components/SelectProfiles.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Profiles = () => {
+    return (
+        <div>Profiles</div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Chackra Provider
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>,
   document.getElementById('root')
 );
 
