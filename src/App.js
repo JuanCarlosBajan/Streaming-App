6ba8bf03b0e053072e7eb172ce4fcd0ade7237ef
Guillermo Santos <34350127+guillermoSb@users.noreply.github.com>
2022-03-23 16:25:29 -0600

    Revert "Saving user when login"

--- src/App.js
+++ src/App.js
@@ -3,33 +3,26 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import Login from './components/Login';
 import { Register } from './components/Register';
 import { Profiles } from './components/SelectProfiles';
-import { useState } from 'react';
 
 //App View
 
 function App() {
-  const [user, setUser] = useState({});
-
-  const userLogInSuccess = (user) => {
-    setUser(user);
-  }
-
   return (
-    <Tabs variant='soft-rounded' colorScheme='green' isLazy={true}>
+    <Tabs variant='soft-rounded' colorScheme='green'>
       <TabList mb='1em'>
         <Tab>Login</Tab>
-        <Tab >Register</Tab>
+        <Tab>Register</Tab>
         <Tab>Select Profiles</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
-          <Login onSuccess={userLogInSuccess} />
+          <Login />
         </TabPanel>
         <TabPanel>
           <Register />
         </TabPanel>
-        <TabPanel >
-          <Profiles user={user} />
+        <TabPanel>
+          <Profiles />
         </TabPanel>
       </TabPanels>
     </Tabs>
