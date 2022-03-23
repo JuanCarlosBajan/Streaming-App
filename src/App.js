9938e48dbf3274ed651cea7e4aa6d12bce548f44
Guillermo Santos <guillermo.santosba@gmail.com>
2022-03-23 16:23:33 -0600

    Saving user when login

--- src/App.js
+++ src/App.js
@@ -3,26 +3,33 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import Login from './components/Login';
 import { Register } from './components/Register';
 import { Profiles } from './components/SelectProfiles';
+import { useState } from 'react';
 
 //App View
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const userLogInSuccess = (user) => {
+    setUser(user);
+  }
+
   return (
-    <Tabs variant='soft-rounded' colorScheme='green'>
+    <Tabs variant='soft-rounded' colorScheme='green' isLazy={true}>
       <TabList mb='1em'>
         <Tab>Login</Tab>
-        <Tab>Register</Tab>
+        <Tab >Register</Tab>
         <Tab>Select Profiles</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
-          <Login />
+          <Login onSuccess={userLogInSuccess} />
         </TabPanel>
         <TabPanel>
           <Register />
         </TabPanel>
-        <TabPanel>
-          <Profiles />
+        <TabPanel >
+          <Profiles user={user} />
         </TabPanel>
       </TabPanels>
     </Tabs>
